a0eb2ed97b2af86838ee3ab1d85bd6015e7db67b
Sam Ferebee <sam.ferebee@gmail.com>
2021-02-22 09:42:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let arr = [];
+    for (let i = 0; i< names.length; i++){
+        console.log(names[i]);
+        arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return arr;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
